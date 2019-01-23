79af19b6ae0a087c93b753edb89dc27feba53b1c
Bryson Nelson <brysonnelson@Brysons-MBP.attlocal.net>
2019-01-22 17:48:44 -0800

    Updates

---
+++ precache-manifest.5a13407a7fbcf00eb52951d50041f3b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nyt-google-books-search/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "15e9a9cedd5b85604ae2",
+    "url": "/nyt-google-books-search/static/js/runtime~main.15e9a9ce.js"
+  },
+  {
+    "revision": "c8e1c2b4f0d145452dc3",
+    "url": "/nyt-google-books-search/static/js/main.c8e1c2b4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/nyt-google-books-search/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c8e1c2b4f0d145452dc3",
+    "url": "/nyt-google-books-search/static/css/main.d10f8194.chunk.css"
+  },
+  {
+    "revision": "806bc83e3d001beb67209865c6b0180d",
+    "url": "/nyt-google-books-search/index.html"
+  }
+];
